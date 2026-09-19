fe7d0443157dc69fc2f30fe81447ac7695c0c46a
Sathvik <tssathvik@gmail.com>
2026-09-19 09:03:05 +0530

    Add app icon (Android mipmaps + web icons); move MainActivity to com.sathvik.musiccollection

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.music_collection"
+    namespace = "com.sathvik.musiccollection"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.music_collection"
+        applicationId = "com.sathvik.musiccollection"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
